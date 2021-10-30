8194d74a5eec66c6883d4a36d914544198945f1b
Florian Bouron <bouron.florian@yahoo.fr>
2021-10-30 09:30:50 +0200

    Add frontend repository

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
+          <p className="sub-text">Team up to protect the Metaverse!</p>
+          <div className="connect-wallet-container">
+            <img
+              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+              alt="Monty Python Gif"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
